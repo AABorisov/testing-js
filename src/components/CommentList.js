9c8074fa92bff4bc94bce0a43d8b33a9c41f5743
AABorisov <borisov.andrew.a@gmail.com>
2018-10-02 13:33:47 +0300

    Router Route exactly by "/" to CommentList Route by "/post" to CommentBox

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -9,9 +9,12 @@ class CommentList extends Component {
     }
     render() {
         return (
+            <div>
+                <h4>Comment List</h4>
                 <ul>
                     {this.renderComments()}
                 </ul>
+            </div>
         );
     }
 }
